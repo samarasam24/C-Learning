386464e8f22b793b062d2fb24691373a15d57275
samarasam24 <samarasamtmk2003@gmail.com>
2024-09-28 06:47:31 +0530

    var dataType tc

--- LearningPhaseOne/src/FIrst.cs
+++ LearningPhaseOne/src/FIrst.cs
@@ -1,8 +1,59 @@
-class FirstProgram {
-    static void Main(string[] args){
-        Secondary();
-    }
-    static void Secondary(){
-        System.Console.WriteLine("Monkey D Luffy");
-    }
-}
+// variables
+// char letter = 'a';
+// int num = 10;
+// double doubleNum = 10.20;
+// string userName = "John";
+//declaring a empty variable
+// bool isTrue;
+//assigning empty variable value
+// isTrue = false; 
+// Console.WriteLine(userName);
+// Console.WriteLine(letter);
+// Console.WriteLine(num);
+// Console.WriteLine(doubleNum);
+// Console.WriteLine(isTrue);
+
+// constants
+// const string passKey = "004-user";
+// Console.WriteLine(passKey);
+
+// display variables
+// Console.WriteLine("string - " + userName );
+// Console.WriteLine(num + doubleNum );
+
+// declare multiple variables
+// int x = 2, y = 4, z = 6;
+// Console.WriteLine( x + y + z);  
+
+// data types
+
+// int 4 bytes
+// long 8 bytes
+// float 4 bytes
+// double 8 bytes
+// bool 1 bit
+// char 2 bytes
+// string 2 bytes per character
+
+// type casting
+
+// implicit casting 
+// int myInt = 9;
+// double myDouble = myInt;  //automatic casting int to double
+// Console.WriteLine(myDouble);
+
+// explicit casting
+// double myDouble = 9;
+// int myInt = (int) myDouble;  //manual casting double to int
+// Console.WriteLine(myInt);
+
+// type conversion methods 
+
+// Convert.ToBoolean()
+// Convert.ToDouble()
+// Convert.ToString()
+// Convert.ToInt32()
+// Convert.ToInt64() etc.
+
+// int myInt = 10;
+// Console.WriteLine(System.Convert.ToString(myInt));
